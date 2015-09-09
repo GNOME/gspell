d75fcff4f860f8576ea95938d8bfc69213d6f164
Sébastien Wilmet <swilmet@gnome.org>
2015-09-09 17:54:28 +0200

    Add gspell.h, the single header to include in an app
    What is still needed is to disallow including other headers.

---
+++ gspell/gspell.h
@@ -0,0 +1,28 @@
+/*
+ * This file is part of gspell.
+ *
+ * Copyright 2015 - Sébastien Wilmet <swilmet@gnome.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gspell/gedit-automatic-spell-checker.h>
+#include <gspell/gedit-spell-checker.h>
+#include <gspell/gedit-spell-checker-dialog.h>
+#include <gspell/gedit-spell-checker-language.h>
+#include <gspell/gedit-spell-language-dialog.h>
+#include <gspell/gedit-spell-navigator.h>
+#include <gspell/gedit-spell-navigator-gtv.h>
+
+/* ex:set ts=8 noet: */
