d6b770e949066fb543c9a443f8e2c11188c69cb9
Sébastien Wilmet <swilmet@mailfence.com>
2024-08-06 15:06:15 +0200

    License headers: apply SPDX-License-Identifier tag
    REUSE way of applying licenses:
    https://reuse.software/
    
    This was done with gcu-smart-c-comment-substitution, part of
    gdev-c-utils:
    https://gitlab.gnome.org/swilmet/gdev-c-utils

--- gspell/gspell-text-view.h
+++ gspell/gspell-text-view.h
@@ -3,18 +3,7 @@
  *
  * Copyright 2015, 2016 - Sébastien Wilmet
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #ifndef GSPELL_TEXT_VIEW_H
